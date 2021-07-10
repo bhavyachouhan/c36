9a0a591a460f4bd7565803aa0b6e6e4b47fccc70
bhavyachouhan <82265304+bhavyachouhan@users.noreply.github.com>
2021-07-10 16:31:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var canvas,backgroundImage,database;
+var gameState = 0;
+var playerCount;
+var form,player,game
+function setup(){
+    database=firebase.database();
+    canvas = createCanvas(400,400);
+    game = new Game();
+    game.getState();
+    game.Start();
+    //ball = createSprite(250,250,10,10);
+    //ball.shapeColor = "red";
+
+    //var ballposition = database.ref('ball/position');
+    //ballposition.on("value",readposition,showerror);
+}
+
+function draw(){
+    background("white");
+    drawSprites();
+}
